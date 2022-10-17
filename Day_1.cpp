7612358208ed3a3b782ff15a68c41c4da8a9f618
Ashutosh <ashuhimself@outlook.com>
2022-10-18 03:01:31 +0530

    first

---
+++ Day_1.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     cout<<"Ashutosh";
+//     return 0;
+// }
+
+// C++ OUTPUT
+
+// is used to get output COUT
+
+int main()
+{
+    cout << "Hello! This is the ";
+    cout << "text we want our users to see.";
+  return 0;
+}
+
+// Variable 
+
+// type variable_name = value
+
+// Identifier is the name of variable classs methods
+
+
+
+
---
+++ a2.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+using namespace std;
+//Swaping the variable
+// int main(){
+//     int a,b;
+//     cout<<"Enter thr value of a"<<endl;
+//     cin>>a;
+//     cout<<"Enter thr value of b"<<endl;
+//     cin>>b;
+//     int c =a;
+//     a=b;
+//     b=c;
+//     cout<<"Value of a is "<<a<<" Value of b is: "<<b<<endl;
+//     return 0;
+// }
+
+// int main(){
+//     int x,y,z;
+//     cin>>x;
+//     cin>>y;
+//     z = x*y;
+//     cout<<"The multiplication is of x and y is "<<z<<endl;
+// }
+
+// int main()
+// {
+// cout << "ASCII value of u is " << (int)'U' << endl;
+//  return 0;
+// }
+int main(){
+    cout << "\n The size of char " <<sizeof(char)<<" Bytes \n"<<endl;
+    cout << "\n The size of float " <<sizeof(float)<<" Bytes \n"<<endl;
+    cout << "\n The size of int " <<sizeof(int)<<" Bytes \n"<<endl;
+    cout << "\n The size of long " <<sizeof(long)<<" Bytes \n"<<endl;
+    cout << "\n The size of long long " <<sizeof(long long)<<" Bytes \n"<<endl;
+    cout << "\n The size of double " <<sizeof(double)<<" Bytes \n"<<endl;
+    cout << "\n The size of bool "<<sizeof(bool)<<" Bytes \n"<<endl;
+    cout << "\n The size of long double " <<sizeof(long double)<<" Bytes \n"<<endl;
+    
+
+}
